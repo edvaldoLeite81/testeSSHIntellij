45615334c36fff053cfaac0704e4327bdd6fef56
edvaldoLeite81 <eguilhermeleite@uni9.edu.br>
2023-12-03 01:55:59 -0300

    update

--- src/main/java/com/example/testesshuni/TesteSshUniApplication.java
+++ src/main/java/com/example/testesshuni/TesteSshUniApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TesteSshUniApplication {
 
     public static void main(String[] args) {
+        System.out.println("Hello GitHub");
         SpringApplication.run(TesteSshUniApplication.class, args);
     }
 
