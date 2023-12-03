3150ae7ce4c9f55a574381ca5d411324e675a677
edvaldoLeite81 <eguilhermeleite@uni9.edu.br>
2023-12-03 01:53:09 -0300

    created

---
+++ src/main/java/com/example/testesshuni/TesteSshUniApplication.java
@@ -0,0 +1,13 @@
+package com.example.testesshuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteSshUniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteSshUniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testesshuni/TesteSshUniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testesshuni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteSshUniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
